2c5c2dd77af448d5dca42ae1a24dbb1f417aa873
Nikolas9896 <nhlynka@gmail.com>
2014-03-14 16:35:02 +0200

    first commit before structure

---
+++ main.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+ int main()
+
+{
+
+  FooIn ( pbase );
+  FooSort( pbase );
+  FooOut( pbase );
+
+  return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H 1
+
+	#include<stdio.h>
+	void FooIn( struct Data *pbase );
+	void FooSort( struct Data *pbase );
+	void FooOut( struct Data *pbase );
+	
+#endif	
